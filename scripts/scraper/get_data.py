c03611dda4d6aeb8d055cd1b041a28f1e2ef971c
Arthur Chieppe <62956486+arthurchieppe@users.noreply.github.com>
2024-08-28 22:08:45 -0300

    organizing project

--- scripts/scraper/get_data.py
+++ scripts/scraper/get_data.py
@@ -2,7 +2,8 @@ import logging
 from ReviewLinkScraper import ReviewLinkScraper
 from ReviewContentScraper import ReviewContentScraper
 from datetime import datetime
-import pandas as pd
+
+# import pandas as pd
 
 
 def save_to_csv(df, filename) -> None:
@@ -17,8 +18,8 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     # Get all review links from the first 75 pages
-    # links_df = ReviewLinkScraper.get_all_review_links(range(1, 3))
-    links_df = pd.read_csv("reviews_links.csv")
+    links_df = ReviewLinkScraper.get_all_review_links(range(1, 3))
+    # links_df = pd.read_csv("reviews_links.csv")
     save_to_csv(links_df, "reviews_links.csv")
     # Get only 5 first rows of df
     reviews_df = ReviewContentScraper.scrape_reviews(links_df)
